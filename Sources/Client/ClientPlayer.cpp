1d214677ffcf97594ba4b0405d9577a3258620a0
Francois ND <francoisnd1@gmail.com>
2026-02-02 22:58:33 +0100

    remove unused variables in Client files

--- Sources/Client/ClientPlayer.cpp
+++ Sources/Client/ClientPlayer.cpp
@@ -611,7 +611,6 @@ namespace spades {
 			Player& p = player;
 			Weapon& w = p.GetWeapon();
 			IRenderer& renderer = client.GetRenderer();
-			World* world = client.GetWorld();
 			Matrix4 eyeMatrix = GetEyeMatrix();
 			Vector3 vel = p.GetVelocity();
 
