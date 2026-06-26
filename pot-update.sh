1dc43ed53a80c843956d13d077dd02535690b255
siecvi <25997662+siecvi@users.noreply.github.com>
2026-06-26 17:15:31 -0300

    add `pot-generate.sh` and rename `update-pot.sh` to `pot-update.sh`

--- pot-update.sh
+++ pot-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This file should be run from the repository root (e.g. ~/openspades)
+# This file should be run from the repository root (e.g. ~/zerospades)
 # TODO: Optimize and error-checking
 
   FILES_H=`find . -iname *.h`
@@ -10,13 +10,13 @@ FILES_CPP=`find . -iname *.cpp`
 FILES="${FILES_H} ${FILES_CPP} ${FILES_C} ${FILES_AS}"
 echo $FILES| tr " " "\n" > .translate.this # Convert spaces to newlines
 
-OPTIONS_OUTPUT="-o Resources/Locales/pot/openspades.pot"
+OPTIONS_OUTPUT="-o Resources/Locales/pot/zerospades.pot"
 OPTIONS_CPP="--c++"
 OPTIONS_KEYWORD="-k_Tr:2,1c -k_TrN:2,1c,3" # Have no idea how this works
 OPTIONS_COMMENTS="-c!" # comments for translators
 OPTIONS="-j ${OPTIONS_OUTPUT} ${OPTIONS_CPP} ${OPTIONS_KEYWORD} ${OPTIONS_COMMENTS}"
 
-META_PKG="--package-name=OpenSpades"
+META_PKG="--package-name=ZeroSpades"
 META_COPYRIGHT="--copyright-holder=yvt"
 META_BUGS="--msgid-bugs-address=i@yvt.jp"
 METADATA="$META_PKG $META_COPYRIGHT $META_BUGS --omit-header"
