0cda7fe7856d3d75330f0f6aa1d3fe83f48e1c1f
Francois ND <francoisnd1@gmail.com>
2026-09-15 21:27:54 +0200

    ui: fix missing cstdint include in TextUtils

--- Sources/Gui/UI/Framework/TextUtils.cpp
+++ Sources/Gui/UI/Framework/TextUtils.cpp
@@ -75,7 +75,7 @@ namespace spades {
 				return ln;
 			}
 
-			std::string FormatFileSize(int64_t bytes) {
+			std::string FormatFileSize(std::int64_t bytes) {
 				if (bytes < 0)
 					return "";
 				if (bytes < 1024)
